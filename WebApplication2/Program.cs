474ebcd245fc49667a763b4b6f2cffb8e1558d59
Sharanu <Sharanubellad1317@gmail.com>
2022-05-31 16:37:05 +0530

    Added Entity Framework and Scoffolded

--- WebApplication2/Program.cs
+++ WebApplication2/Program.cs
@@ -1,8 +1,14 @@
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+using WebApplication2.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+var connString = builder.Configuration.GetConnectionString("BookStoreAppDBConnection");
+
+builder.Services.AddDbContext<BookStoreDBContext>(options => options.UseSqlServer(connString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
